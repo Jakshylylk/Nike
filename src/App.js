f1705d81e68801945c994eaebefcc34afc1321f1
jakshylylk <m.jakshylyk09@gmail.com>
2022-11-04 13:17:08 +0600

    started add product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AddProduct from "./Components/Admin/AddProduct/AddProduct";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AddProduct />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Admin/AddProduct/AddProduct.jsx
+++ src/Components/Admin/AddProduct/AddProduct.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./AddProduct.css";
 
 const AddProduct = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="addProduct-ontainer">
+        <input type="text" placeholder="Категория" />
+        <input type="text" placeholder="Характеристика" />
+        <input type="text" placeholder="Описания" />
+        <input type="text" placeholder="Цвет" />
+        <input type="number" placeholder="Цена" />
+        <input type="text" placeholder="Добавить 1 картинку" />
+        <input type="text" placeholder="Добавить 2 картинку" />
+        <input type="text" placeholder="Добавить 3 картинку" />
+        <input type="text" placeholder="Добавить 4 картинку" />
+      </div>
+    </>
+  );
 };
 
 export default AddProduct;
